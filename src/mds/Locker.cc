e807ebabdaee11679663a2698a270e08d4bf5e77
Yan, Zheng <yanzheng03@kuaishou.com>
2021-01-06 16:46:47 +0800

    mds: don't evict importing session when revoking stale write caps
    When the MDS detects a stale session during recovery and revokes its
    capabilities, it previously treated any session holding write caps as
    a candidate for eviction.  However, a session that is in the process
    of being imported from another MDS may hold write caps on inodes that
    are being migrated along with it.  Evicting an importing session at
    this point interrupts the migration and can leave the inode in an
    inconsistent half-imported state.  Track whether the session is
    importing and skip eviction for write-cap holders when the session is
    actively being transferred, allowing the import to complete normally
    before the session's caps are re-evaluated.
    
    Fixes: https://tracker.ceph.com/issues/77427
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    Signed-off-by: Xiubo Li <xiubo.li@clyso.com>

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -2742,6 +2742,7 @@ bool Locker::revoke_stale_caps(Session *session)
 
   // invalidate all caps
   session->inc_cap_gen();
+  bool can_evict = !session->is_importing();
 
   bool ret = true;
   std::vector<CInode*> to_eval;
@@ -2760,7 +2761,7 @@ bool Locker::revoke_stale_caps(Session *session)
     if (!revoking)
       continue;
 
-    if (revoking & CEPH_CAP_ANY_WR) {
+    if ((revoking & CEPH_CAP_ANY_WR) && can_evict) {
       ret = false;
       break;
     }
