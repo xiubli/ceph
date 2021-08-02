232b9fe08201d4891b35ea0d7304cef0cfc7575a
Xiubo Li <xiubli@redhat.com>
2021-08-02 23:19:16 +0800

    common/fair_mutex: support ceph_condition_variable
    Add _post_lock(), _pre_unlock and native_handle() support, and at
    the same time switch ceph::mutex/ceph::condition_variable to std
    ones.
    
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/common/fair_mutex.h
+++ src/common/fair_mutex.h
@@ -7,17 +7,44 @@
 #include <pthread.h>
 #include <thread>
 #include <string>
+#include <mutex>
 
 namespace ceph {
 /// a FIFO mutex
 class fair_mutex {
 public:
-  fair_mutex(const std::string& name)
-    : mutex{ceph::make_mutex(name)}
-  {}
+  fair_mutex(const std::string& name) {}
   ~fair_mutex() = default;
   fair_mutex(const fair_mutex&) = delete;
   fair_mutex& operator=(const fair_mutex&) = delete;
+
+  pthread_mutex_t* native_handle() {
+    return mutex.native_handle();
+  }
+
+  pthread_cond_t* native_cond_handle() {
+    return cond.native_handle();
+  }
+
+  void _pre_unlock() {
+    mutex.lock();
+    ceph_assert(locked_by == std::this_thread::get_id());
+    ++unblock_id;
+    locked_by = std::thread::id();
+    // wake up all the waiters before giving up the lock
+    // in ceph_condition_variable's wait()
+    int r = pthread_cond_broadcast(cond.native_handle());
+    ceph_assert(r == 0);
+  }
+
+  void _post_lock() {
+    const unsigned my_id = next_id++;
+    while (my_id != unblock_id) {
+      pthread_cond_wait(cond.native_handle(), mutex.native_handle());
+    }
+    locked_by = std::this_thread::get_id();
+    mutex.unlock();
+  }
 
   void lock()
   {
@@ -60,8 +87,8 @@ public:
 private:
   unsigned next_id = 0;
   unsigned unblock_id = 0;
-  ceph::condition_variable cond;
-  ceph::mutex mutex;
+  std::condition_variable cond;
+  std::mutex mutex;
   std::thread::id locked_by = {};
 };
 } // namespace ceph
