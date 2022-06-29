019ef81907b361622021d6f745ac5a022feab5f4
Milind Changire <mchangir@redhat.com>
2022-06-29 15:26:44 +0530

    mds/scrub: move inline data to data pool object
    If inline data version is not CEPH_INLINE_NONE then move data to data
    pool object and set inline data version to CEPH_INLINE_NONE.
    
    Fixes: https://tracker.ceph.com/issues/52916
    Signed-off-by: Milind Changire <mchangir@redhat.com>

--- src/mds/MDCache.h
+++ src/mds/MDCache.h
@@ -958,6 +958,8 @@ class MDCache {
   void repair_dirfrag_stats(CDir *dir);
   void rdlock_dirfrags_stats(CInode *diri, MDSInternalContext *fin);
 
+  void uninline_data_work(MDRequestRef mdr);
+
   // my leader
   MDSRank *mds;
 
