1d5cfbff5039e702c27e5a503081eb29496d8c6b
Xiubo Li <xiubli@redhat.com>
2022-09-09 11:36:31 +0800

    client: check the cephx mds auth access for setattr
    Fixes: https://tracker.ceph.com/issues/57154
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -7845,6 +7845,8 @@ int Client::_do_setattr(Inode *in, struct ceph_statx *stx, int mask,
   bool kill_sguid = false;
   int inode_drop = 0;
   size_t auxsize = 0;
+  filepath path;
+  MetaRequest *req;
 
   if (aux)
     auxsize = aux->size();
@@ -7891,6 +7893,11 @@ int Client::_do_setattr(Inode *in, struct ceph_statx *stx, int mask,
      * infrastructure...
      */
     mask |= CEPH_SETATTR_CTIME;
+  }
+
+  int res = mds_check_access(in, perms, MAY_WRITE);
+  if (res) {
+    goto out;
   }
 
   if (!mask) {
@@ -8135,9 +8142,7 @@ int Client::_do_setattr(Inode *in, struct ceph_statx *stx, int mask,
     return 0;
   }
 
-  MetaRequest *req = new MetaRequest(CEPH_MDS_OP_SETATTR);
-
-  filepath path;
+  req = new MetaRequest(CEPH_MDS_OP_SETATTR);
 
   in->make_nosnap_relative_path(path);
   req->set_filepath(path);
@@ -8153,7 +8158,9 @@ int Client::_do_setattr(Inode *in, struct ceph_statx *stx, int mask,
   req->head.args.setattr.mask = mask;
   req->regetattr_mask = mask;
 
-  int res = make_request(req, perms, inp);
+  res = make_request(req, perms, inp);
+
+out:
   ldout(cct, 10) << "_setattr result=" << res << dendl;
   return res;
 }
