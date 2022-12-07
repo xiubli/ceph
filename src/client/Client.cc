6421cbba51d35ffdf8a6a6db0bff2e3ded46ff17
Xiubo Li <xiubli@redhat.com>
2022-12-07 15:03:35 +0800

    client: save the cap_auths in client when session being opened
    Fixes: https://tracker.ceph.com/issues/57154
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -2403,6 +2403,7 @@ void Client::handle_client_session(const MConstRef<MClientSession>& m)
 
       session->mds_features = std::move(m->supported_features);
       session->mds_metric_flags = std::move(m->metric_spec.metric_flags);
+      cap_auths = std::move(m->cap_auths);
 
       renew_caps(session.get());
       session->state = MetaSession::STATE_OPEN;
@@ -5813,6 +5814,46 @@ void Client::handle_cap_grant(MetaSession *session, Inode *in, Cap *cap, const M
   // may drop inode's last ref
   if (deleted_inode)
     _try_to_trim_inode(in, true);
+}
+
+int Client::mds_check_access(Inode *in, const UserPerm& perms, int mask)
+{
+  const gid_t *gids;
+  int count = perms.get_gids(&gids);
+  std::vector<uint64_t> gid_list;
+  filepath path;
+
+  ldout(cct, 25) << in << ", uid " << perms.uid() << ", gid " << perms.gid()
+                 << ", mask " << mask << dendl;
+  in->make_long_path(path);
+  if (count) {
+    gid_list.reserve(count);
+    for (int i = 0; i < count; ++i) {
+      gid_list.push_back(gids[i]);
+    }
+  }
+
+  for (auto& s: cap_auths) {
+    if (s.match.match(path.get_path(), perms.uid(), perms.gid(), &gid_list)) {
+      if ((mask & MAY_WRITE) && s.writeable &&
+	  s.match.root_squash && ((perms.uid() == 0) || (perms.gid() == 0))) {
+        ldout(cct, 10) << " permission denied, root_squash is enabled and user"
+                       << " (uid " << perms.uid() << ", gid " << perms.gid()
+                       << ") isn't allowed to write" << dendl;
+        return -CEPHFS_EACCES;
+      }
+
+      if (((mask & MAY_WRITE) && !s.writeable) ||
+          ((mask & MAY_READ) && !s.readable)) {
+        ldout(cct, 10) << " permission denied, mds auth caps readable/writeable:"
+                       << s.readable << "/" << s.writeable << ", request r/w:"
+                       << (mask & MAY_READ) << "/" << (mask & MAY_WRITE) << dendl;
+        return -CEPHFS_EACCES;
+      }
+    }
+  }
+
+  return 0;
 }
 
 int Client::inode_permission(Inode *in, const UserPerm& perms, unsigned want)
