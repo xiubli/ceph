af3c53ea285a404a6ef0e2e0278342e5dca8f58f
Xiubo Li <xiubo.li@clyso.com>
2026-06-29 13:31:19 +0800

    mds: add mds_dir_prefetch_backend for non-blocking dirfrag prefetch
    mds_dir_prefetch_backend bridges the performance gap between
    mds_dir_prefetch=true (blocking full-omap fetch, ~95 ms for random)
    and mds_dir_prefetch=false (keyed-only fetch, ~3.7 ms no cache warmth).
    
    When mds_dir_prefetch=false and mds_dir_prefetch_backend=true, every
    dentry lookup completes via a fast targeted keyed OSD fetch while
    a background full dirfrag prefetch warms the cache for subsequent
    accesses.  mds_dir_prefetch_backend_max (default 1) limits concurrent
    background fetches to avoid OSD I/O contention.
    
    Fixes: https://tracker.ceph.com/issues/77810
    Signed-off-by: Xiubo Li <xiubo.li@clyso.com>

--- src/mds/MDSRank.cc
+++ src/mds/MDSRank.cc
@@ -3726,6 +3726,8 @@ void MDSRank::create_logger()
 			    "dir_fetch_complete", "Fetch complete dirfrag");
     mds_plb.add_u64_counter(l_mds_dir_fetch_keys,
 			    "dir_fetch_keys", "Fetch keys from dirfrag");
+    mds_plb.add_u64_counter(l_mds_dir_fetch_background,
+			    "dir_fetch_background", "Background full dirfrag prefetch");
     mds_plb.add_u64_counter(l_mds_dir_commit, "dir_commit", "Directory commit");
     mds_plb.add_u64_counter(l_mds_dir_split, "dir_split", "Directory split");
     mds_plb.add_u64_counter(l_mds_dir_merge, "dir_merge", "Directory merge");
@@ -4098,6 +4100,9 @@ std::vector<std::string> MDSRankDispatcher::get_tracked_keys()
     "mds_cap_revoke_eviction_timeout",
     "mds_debug_subtrees",
     "mds_dir_max_entries",
+    "mds_dir_prefetch",
+    "mds_dir_prefetch_backend",
+    "mds_dir_prefetch_backend_max",
     "mds_dump_cache_threshold_file",
     "mds_dump_cache_threshold_formatter",
     "mds_enable_op_tracker",
